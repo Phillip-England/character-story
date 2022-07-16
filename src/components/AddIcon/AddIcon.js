ca03e8cae233793f187daced63f55573209dbc21
phillipmarkengland <phillipmarkengland@penguin.lxd>
2022-07-16 06:07:28 -0500

    init

---
+++ src/components/AddIcon/AddIcon.js
@@ -0,0 +1,7 @@
+import styles from './AddIcon.module.css'
+
+export default function AddIcon(){
+    return(
+        <p>icon</p>
+    )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+    return(
+        <h1 className={styles.header}>Character Story</h1>
+    )
+}
